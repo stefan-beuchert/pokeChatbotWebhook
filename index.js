e1aabb9bfb6a77fa701bdbb9ddbf8ddd30fe8406
dida1012 <daviddiener@outlook.com>
2019-04-19 00:20:48 +0200

    added pokeapi fetch

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express    = require('express');   
 var bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 var app        = express();
 
 app.use(basicAuth({
@@ -9,52 +11,40 @@ app.use(basicAuth({
     },
     challenge: true
 }));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 8080;        // set our port
-var router = express.Router();              // get an instance of the express Router
-
-router.get('/', function(req, res) {
-    res.json({ fulfillmentText: 'hooray! welcome to our api!' });
-});
+var port = process.env.PORT || 8080;        // set port
+var router = express.Router();              // get instance of express Router
 
 router.post('/', function(req, res) {
-    res.json({ fulfillmentText: 'hooray! welcome to our api! -->POST' });
+    var request = new XMLHttpRequest()
+    var intent = req.body.queryResult.intent['displayName'];
+    var pokemon = req.body.queryResult.parameters['pokemon'];
+    pokemon = pokemon.charAt(0).toLowerCase() + pokemon.slice(1);
+    request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+pokemon+'/', true)
+  
+    request.onload = function() {
+      if (request.status >= 200 && request.status < 400) {
+        var data = JSON.parse(this.responseText)
+        pokemon = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);
+        var output = '';
+        switch(intent){
+          case "Abilities": output = pokemon+" has the Ability '"+data.abilities[0].ability.name+"'."; break;
+          case "Height": output = pokemon+" is "+data.height+" meters tall."; break;
+          default: output = "No Intent parsed"; break;
+        }
+      } else {
+        output = "There is no Pokemon with the name "+pokemon+". Check your damn Pokedex!";
+        }
+        res.json({ fulfillmentText: output});
+    }
+    request.send()
 });
 
 app.use('/api', router);
 
 // START THE SERVER
 app.listen(port);
-console.log('Magic happens on port ' + port);
-
-
-
-// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-// var request = new XMLHttpRequest()
-
-// module['exports'] = function whatthefuck (hook) {  //Funktionsname ändern wenn komischer Fehler auftritt
-//   var intent = hook.req.body.queryResult.intent['displayName'];
-//   var pokemon = hook.req.body.queryResult.parameters['pokemon'];
-//   pokemon = pokemon.charAt(0).toLowerCase() + pokemon.slice(1);
-//   request.open('GET', 'https://pokeapi.co/api/v2/pokemon/'+pokemon+'/', true)
-
-//   request.onload = function() {
-//     if (request.status >= 200 && request.status < 400) {
-//       var data = JSON.parse(this.responseText)
-//       pokemon = pokemon.charAt(0).toUpperCase() + pokemon.slice(1);
-//       var output = '';
-//       switch(intent){
-//         case "Abilities": output = pokemon+" has the Ability '"+data.abilities[0].ability.name+"'."; break;
-//         case "Height": output = pokemon+" is "+data.height+" meters tall."; break;
-//         default: output = "No Intent parsed"; break;
-//       }
-//     } else {
-//       output = "There is no Pokemon with the name "+pokemon+". Check your damn Pokedex!";
-//       }
-//     hook.res.json({"fulfillmentText": output});
-//     hook.res.end();
-//   }
-//   request.send()
-// }
+console.log('Started Server on Port: ' + port);
