f0fd8c7dca97b878a4cbe38b8789947defa73bf6
dida1012 <daviddiener@outlook.com>
2019-05-06 23:49:08 +0200

    fixed bugs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-require('./imageProcessing')
+var Jimp = require('jimp');
+var GIFEncoder = require('gifencoder');
+var fs = require('fs');
 
 var app = express();
 
@@ -32,6 +34,27 @@ router.post('/', function(req, res) {
 
         // var image = data.sprites.front_default;
         // var image = 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/23c3b120-e209-4850-9d73-af3fe703e962/dcybyvi-e4924343-e5dd-4e04-826c-8a9287f92c0c.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzIzYzNiMTIwLWUyMDktNDg1MC05ZDczLWFmM2ZlNzAzZTk2MlwvZGN5Ynl2aS1lNDkyNDM0My1lNWRkLTRlMDQtODI2Yy04YTkyODdmOTJjMGMuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PDmy73FKHdsz9fbaJwJf1m4kQi3QgHcGRt1jpgWgSi8';
+
+        var encoder = new GIFEncoder(921, 506);
+        encoder.createReadStream().pipe(fs.createWriteStream('./resources/final.gif'));
+
+        encoder.start();
+        encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat 
+        encoder.setDelay(200);  // frame delay in ms 
+        encoder.setQuality(10); // image quality. 10 is default. 
+
+        function processFrame(background, pkmn){
+            Jimp.read(pkmn, (err, riolu) => {
+                if (err) throw err;
+                riolu
+                .resize(100,100)
+                Jimp.read(background, (err, oak) => {
+                    if (err) throw err;
+                    oak.composite(riolu, 500, 100);
+                    encoder.addFrame(oak.bitmap.data)
+                });
+            });
+        }
 
         for(i=0;i<10;++i){
           processFrame('./resources/oak_0'+i+'.png', './resources/riolu.png')
