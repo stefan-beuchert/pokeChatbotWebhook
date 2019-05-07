8e9f32fec674672671bb592a9410681b805c14d3
dida1012 <daviddiener@outlook.com>
2019-05-07 19:15:50 +0200

    deleted riolu.png

--- index.js
+++ index.js
@@ -45,11 +45,11 @@ router.post('/', function(req, res) {
             Jimp.read(pkmn, (err, pkmn) => {
                 if (err) throw err;
                 pkmn
-                .resize(300,300)
+                .resize(50,50)
                 Jimp.read(background, (err, background) => {
                     if (err) throw err;
                     background
-                    .composite(pkmn, 500, 100)
+                    .composite(pkmn, 0, 0)
                     .resize(400,250);
                     encoder.addFrame(background.bitmap.data)
                 });
