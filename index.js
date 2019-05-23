20e34f47640b12b2b08db4ca040a33f955654090
buja101 <39958245+buja101@users.noreply.github.com>
2019-05-23 21:43:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ router.post('/', function(req, res) {
             }
         break;
         case "Succesful": 
-            var typedata = helper.getData(url = 'https://pokeapi.co/api/v2/type/' + data.types[0].type.name);
+            var typedata = helper.getData(url = data.types[0].type.url);
                 output = pokemon + " gives double damage to " + typedata.damage_relations.double_damage_to[0].name;
             break;            
         default: output = "No Intent parsed"; break;
